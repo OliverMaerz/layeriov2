0c90ef3e3c744b4fe743900b5475b5a4aa938069
Oliver Maerz <om@olivermaerz.com>
2019-05-18 15:30:18 -0400

    Add regex check and classes for data

--- main.py
+++ main.py
@@ -1,7 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List  # Set, Dict, Tuple, Optional
 from pydantic import BaseModel, EmailStr
 
+from sqlalchemy import Boolean, Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import Session, sessionmaker
+
+from starlette.requests import Request
+from starlette.responses import Response
+
+# import re
+import pcre
+
+import logging
+
+SQLALCHEMY_DATABASE_URI = "mysql://layeriou:tnQtS0jKmUpEp2tb9HSVX8gI9PgBwc@localhost/layerio"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class CustomBase:
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+
+Base = declarative_base(cls=CustomBase)
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+
+
+class Sharing(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    from_x = Column(Integer, unique=False, index=False)
+    from_y = Column(Integer, unique=False, index=False)
+    to_x = Column(Integer, unique=False, index=False)
+    to_y = Column(Integer, unique=False, index=False)
+
+
+class User2Sharing(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False, unique=False, index=True)
+    sharing_id = Column(Integer, ForeignKey("sharing.id"), nullable=False, unique=False, index=True)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+# db_session = SessionLocal()
+
+# first_user = db_session.query(User).first()
+#  if not first_user:
+#    u = User(email="johndoe@example.com", hashed_password="notreallyhashed")
+#    db_session.add(u)
+#    db_session.commit()
+
+# db_session.close()
 
 class MasterSheet:
     """Class to hold the sheet data and manage the shares."""
@@ -51,13 +111,22 @@ class MasterSheet:
             return False
 
 
+#  class Sharings(BaseModel):
+#     sometext: EmailStr
+
+
 class Sharings(BaseModel):
-    sometext: EmailStr
+    emails: List[EmailStr]
+    sheet_selections: List[str]
 
 
 # class Sharings(BaseModel):
 #    sharing: Sharing()
 #    email: str
+
+# Dependency
+def get_db(request: Request):
+    return request.state.db
 
 
 app = FastAPI()
@@ -68,15 +137,35 @@ empty_ms = MasterSheet.empty()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"app": "Simple Sheet Manager", "version": "0.2"}
 
 
 @app.put("/sharing/")
 def add_sharing(sharings: Sharings):
-    return {"success": True, "sometext": sharings.sometext}
+    # check if sharing is valid etc.
+    # raise HTTPException(status_code=404, detail="Item not found")
+    text = None
+    for sheet_selection in sharings.sheet_selections:
+        # white space needed to be removed for pattern matching string without single quotes
+        # regex = pcre.match("^((?>(>?'[\w\s]+')|(>?[\w]+))(?>![A-Z]{1}[0-9]{1})?(?>:[A-Z]{1}[0-9])?)",
+        #                    sheet_selection)
+
+        # Updated regular expression that allows A1:CCC1234567 and also lower case
+        regex = pcre.search(
+            "^((?>(>?'[\w\s]+')|(>?[\w]+))(?>![a-z,A-Z]{1,3}[0-9]{1,7})?(?>:[a-z,A-Z]{1,3}[0-9]{1,7})?)",
+            sheet_selection)
+        if (regex is None) or (regex.group() != sheet_selection):
+            raise HTTPException(status_code=404,
+                                detail="Sheet Selection string is not valid: "+sheet_selection)
+
+        logging.warning('regex results: %s', regex)
+
+    return {"success": True,
+            "emails": sharings.emails,
+            "sheet_selections": sharings.sheet_selections}
 
 
-@app.put("/sharings/")
+@app.get("/sharings/")
 def list_sharings():
     return {"success": True, "sometext": sharings.sometext}
 
@@ -97,10 +186,20 @@ async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q, "mode": "asynchronous"}
 
 
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
+
 # Sheetnames
 # from layer.io
-regex = "^((?>(>?'[\w\s]+')|(>?'[\w\s]+'))(?>![A-Z]{1}[0-9]{1})?(?>:[A-Z]{1}[0-9])?)"
-
+# regex = "^((?>(>?'[\w\s]+')|(>?'[\w\s]+'))(?>![A-Z]{1}[0-9]{1})?(?>:[A-Z]{1}[0-9])?)"
 
 # Files
 # regex filter invalid characters windows [\\/:"*?<>|]+
